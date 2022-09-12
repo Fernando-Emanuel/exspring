6d5c38f8f4324ebdfa6b9f6fcd94f45c592c1524
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-09-12 17:31:38 +0000

    mysql

---
+++ app/src/main/java/exspring/repositories/GeneroRepository.java
@@ -0,0 +1,9 @@
+package exspring.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import exspring.models.Genero;
+
+public class GeneroRepository extends CrudRepository<Genero, Integer> {
+
+}
